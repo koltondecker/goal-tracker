f2059f7a74cd3ca8b0b83b1ec39855115098ac5b
verusbabb <steve.babb@outlook.com>
2021-03-20 13:04:59 -0600

    lint fix

--- models/index.js
+++ models/index.js
@@ -1,6 +1,6 @@
-'use strict';
-var fs        = require('fs');
-var path      = require('path');
+"use strict";
+var fs        = require("fs");
+var path      = require("path");
 var basename  = path.basename(module.filename);
 var db        = {};
 const Sequelize = require("sequelize");
@@ -17,10 +17,10 @@ const sequelize = process.env.JAWSDB_URL
 fs
   .readdirSync(__dirname)
   .filter(function(file) {
-    return (file.indexOf('.') !== 0) && (file !== basename) && (file.slice(-3) === '.js');
+    return (file.indexOf(".") !== 0) && (file !== basename) && (file.slice(-3) === ".js");
   })
   .forEach(function(file) {
-    var model = sequelize['import'](path.join(__dirname, file));
+    var model = sequelize["import"](path.join(__dirname, file));
     db[model.name] = model;
   });
 Object.keys(db).forEach(function(modelName) {
