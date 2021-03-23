f055b45b078b7210a862c70a2593a1aa8fe062bb
Christina <christinalmoss@gmail.com>
2021-03-22 20:20:50 -0500

    migrated routes to handlebars from html

--- public/js/login.js
+++ public/js/login.js
@@ -25,14 +25,14 @@ $(document).ready(() => {
 
 //test
 
-  // loginUser does a post to our "api/login" route and if successful, redirects us the the members page
+  // loginUser does a post to our "api/login" route and if successful, redirects us the the homepage page
   function loginUser(email, password) {
     $.post("/api/login", {
       email: email,
       password: password
     })
       .then(() => {
-        window.location.replace("/members");
+        window.location.replace("/homepage");
         // If there's an error, log the error
       })
       .catch(err => {
--- routes/html-routes.js
+++ routes/html-routes.js
@@ -1,29 +1,36 @@
 // Requiring path to so we can use relative routes to our HTML files
-const path = require("path");
+// const path = require("path");
 
 // Requiring our custom middleware for checking if a user is logged in
 const isAuthenticated = require("../config/middleware/isAuthenticated");
 
 module.exports = function(app) {
   app.get("/", (req, res) => {
-    // If the user already has an account send them to the members page
-    if (req.user) {
-      res.redirect("/members");
-    }
-    res.sendFile(path.join(__dirname, "../public/index.html"));
+    // If the user already has an account send them to the homepage page
+    // if (req.user) {
+    //   res.redirect("/homepage");
+    // }
+    // res.sendFile(path.join(__dirname, "../public/index.html"));
+    res.render("index", {});
   });
 
   app.get("/login", (req, res) => {
-    // If the user already has an account send them to the members page
-    if (req.user) {
-      res.redirect("/members");
-    }
-    res.sendFile(path.join(__dirname, "../public/login.html"));
+    // If the user already has an account send them to the homepage page
+    // if (req.user) {
+    //   res.redirect("/homepage");
+    // }
+    // res.sendFile(path.join(__dirname, "../public/login.html"));
+    res.render("login");
+  });
+
+  app.get("/signup", (req, res) => {
+    res.render("signup");
   });
 
   // Here we've add our isAuthenticated middleware to this route.
   // If a user who is not logged in tries to access this route they will be redirected to the signup page
-  app.get("/members", isAuthenticated, (req, res) => {
-    res.sendFile(path.join(__dirname, "../public/members.html"));
+  app.get("/homepage", isAuthenticated, (req, res) => {
+    // res.sendFile(path.join(__dirname, "../public/homepage.html"));
+    res.render("homepage");
   });
 };
