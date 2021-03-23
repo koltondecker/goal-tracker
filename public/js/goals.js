c011ee2ecbaa4def74118d7a31fc01beef5cbe3e
Kolton Decker <deckerman25@gmail.com>
2021-03-23 18:15:20 -0500

    Added some starter code for adding progress bars for all ongoing goals.

--- public/js/goals.js
+++ public/js/goals.js
@@ -34,4 +34,71 @@ $(document).ready(() => {
         };
 
     }
+
+    $.get("api/all_goals", 
+        console.log("Getting all goals")
+    )
+    .then((goalsData) => {
+        
+        goalsData.forEach(goal => () => {
+            console.log(goal);
+            const options = {
+                chart: {
+                height: 280,
+                type: "radialBar",
+                },
+            
+                series: [goal.goalNumber],
+                colors: ["#20E647"],
+                plotOptions: {
+                radialBar: {
+                    hollow: {
+                    margin: 0,
+                    size: "70%",
+                    background: "#293450"
+                    },
+                    track: {
+                    dropShadow: {
+                        enabled: true,
+                        top: 2,
+                        left: 0,
+                        blur: 4,
+                        opacity: 0.15
+                    }
+                    },
+                    dataLabels: {
+                    name: {
+                        offsetY: -10,
+                        color: "#fff",
+                        fontSize: "13px"
+                    },
+                    value: {
+                        color: "#fff",
+                        fontSize: "30px",
+                        show: true
+                    }
+                    }
+                }
+                },
+                fill: {
+                type: "gradient",
+                gradient: {
+                    shade: "dark",
+                    type: "vertical",
+                    gradientToColors: ["#87D4F9"],
+                    stops: [0, 100]
+                }
+                },
+                stroke: {
+                lineCap: "round"
+                },
+                labels: [goal.goalName]
+            };
+            
+            const chart = new ApexCharts(document.querySelector("#chart"), options);
+            
+            chart.render();
+        });
+    });
+
 });
